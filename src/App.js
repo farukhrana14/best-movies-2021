b99f68a482ce28fcad33301007762006f126739b
Farukhrana14 <farukhrana14@gmail.com>
2021-02-24 01:33:49 +0600

    use of props to pass and render variable names in components

--- src/App.js
+++ src/App.js
@@ -5,21 +5,34 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit done <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <h1>BEST MOVIES OF 2020</h1>
+      <Person name="Wonder Woman" director="Patty Jenkins"></Person>
+      <Person name="The Devil All the Time" director="Antonio Campos"></Person> 
+      <Person name="Gretel Hansel" director="Oz Perkins"></Person> 
+      <Person name="The Invisible Man" director="Leigh Whannell"></Person>  
       </header>
     </div>
   );
+}
+
+
+
+function Person(props) {
+  
+  const personStyle = {
+    border: '2px solid red',
+    borderRadius: '10px',
+    margin: '10px',
+    padding: '10px',
+    width: '500px',
+  }
+ 
+  return (
+  <div style={personStyle}>
+  <h2>Movie: {props.name}</h2>
+  <h3>Director: {props.director}</h3>
+  </div>
+  )
 }
 
 export default App;
